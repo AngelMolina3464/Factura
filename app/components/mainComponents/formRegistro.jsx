96c8e99624a3bc56b7c2e0db05798d6e4f5141eb
AngelMolina3464 <molina_gap@outlook.es>
2024-05-27 13:09:07 -0600

    27-05-2024 Push

--- app/components/mainComponents/formRegistro.jsx
+++ app/components/mainComponents/formRegistro.jsx
@@ -2,11 +2,15 @@
 import { useState } from "react";
 
 export const FormRegistro = () => {
+  const [isActiveContract, setIsActiveContract] = useState(true);
   const [data, setData] = useState({
     concepto: "",
     numeroContrato: "",
-    intervaloContrato: "",
+    iniciodeContrato: "",
+    finaldeContrato: "",
     montoEstimado: 0,
+    iva: 0,
+    total: 0,
   });
 
   const handleClick__SendPreview = () => {
@@ -20,6 +24,31 @@ export const FormRegistro = () => {
     setData({ ...data, numeroContrato: event.target.value });
   };
 
+  const HandleClick__ContratoInicio = (event) => {
+    setData({ ...data, iniciodeContrato: event.target.value });
+  };
+
+  const HandleClick__ContratoFinal = (event) => {
+    setData({ ...data, finaldeContrato: event.target.value });
+  };
+
+  const HandleClick__MontoBruto = (event) => {
+    const montoEstimado = parseFloat(event.target.value);
+    const iva = parseFloat((montoEstimado * 0.16).toFixed(2));
+    const total = parseFloat((montoEstimado + iva).toFixed(2));
+    setData({ ...data, montoEstimado: montoEstimado, iva: iva, total: total });
+  };
+
+  const handle__ActiveButton = (event) => {
+    event.preventDefault();
+    setIsActiveContract("");
+  };
+
+  const handle__InactiveButton = (event) => {
+    event.preventDefault();
+    setIsActiveContract(" ");
+  };
+
   return (
     <section className="registro__Container">
       <form action="">
@@ -29,19 +58,27 @@ export const FormRegistro = () => {
         </div>
         <div className="registro__SubFlex">
           <label htmlFor="">Numero de Contrato </label>
-          <input type="text" onClick={HandleClick__NumContrato} />
+          <input
+            type="text"
+            onChange={HandleClick__NumContrato}
+            disabled={isActiveContract}
+          />
+          <div className="miniButtoner">
+            <button onClick={handle__ActiveButton}>🖋️</button>
+            <button onClick={handle__InactiveButton}>❌</button>
+          </div>
         </div>
 
         <div className="registro__SubFlex">
           <label htmlFor="">Fecha Inicio de Contrato: </label>
-          <input type="date" />
+          <input type="date" onChange={HandleClick__ContratoInicio} />
           <label htmlFor="">Fecha Final de Contrato: </label>
-          <input type="date" />
+          <input type="date" onChange={HandleClick__ContratoFinal} />
         </div>
 
         <div className="registro__SubFlex">
-          <label htmlFor="">Monto Estimado </label>
-          <input type="number" />
+          <label htmlFor="">Monto Estimado Bruto</label>
+          <input type="number" onChange={HandleClick__MontoBruto} />
         </div>
       </form>
 
